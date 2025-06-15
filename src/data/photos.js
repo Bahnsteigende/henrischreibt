2fead7820b8e5c501f1c20197d58e9aa46c3c587
Henri <w.henri@gmx.de>
2025-06-15 20:04:12 +0200

    Meine Änderungen

--- src/data/photos.js
+++ src/data/photos.js
@@ -5,15 +5,15 @@ export const photoSets = [
     category: "events",
     cover: "/bilder/portfolio/Symposium/Symp (5).jpg",
     images: [
-    "/bilder/portfolio/Symposium/Symp (1).jpg",
-    "/bilder/portfolio/Symposium/Symp (2).jpg",
-    "/bilder/portfolio/Symposium/Symp (3).jpg",
-    "/bilder/portfolio/Symposium/Symp (4).jpg",
-    "/bilder/portfolio/Symposium/Symp (5).jpg",
-    "/bilder/portfolio/Symposium/Symp (6).jpg",
-    "/bilder/portfolio/Symposium/Symp (7).jpg",
-    "/bilder/portfolio/Symposium/Symp (8).jpg",
-    "/bilder/portfolio/Symposium/Symp (9).jpg"    
+    "/bilder/portfolio/Symposium/Symp1.jpg",
+    "/bilder/portfolio/Symposium/Symp2.jpg",
+    "/bilder/portfolio/Symposium/Symp3.jpg",
+    "/bilder/portfolio/Symposium/Symp4.jpg",
+    "/bilder/portfolio/Symposium/Symp5.jpg",
+    "/bilder/portfolio/Symposium/Symp6.jpg",
+    "/bilder/portfolio/Symposium/Symp7.jpg",
+    "/bilder/portfolio/Symposium/Symp8.jpg",
+    "/bilder/portfolio/Symposium/Symp9.jpg"    
     ],
     description: "Das xx. Heidelberger Symposium an dessen Planung ich als Teil des Orga-Teams mitgewirkt habe.",
     date: "2025-05-22"
@@ -24,11 +24,11 @@ export const photoSets = [
     category: "outdoor",
     cover: "/bilder/portfolio/Bergsteigen/Bergsteigen (3).jpg",
     images: [
-      "/bilder/portfolio/Bergsteigen/Bergsteigen (1).jpg",
-      "/bilder/portfolio/Bergsteigen/Bergsteigen (2).jpg",
-      "/bilder/portfolio/Bergsteigen/Bergsteigen (3).jpg",
-      "/bilder/portfolio/Bergsteigen/Bergsteigen (4).jpg",
-      "/bilder/portfolio/Bergsteigen/Bergsteigen (5).jpg"      
+      "/bilder/portfolio/Bergsteigen/Bergsteigen1.jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen2.jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen3.jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen4.jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen5.jpg"      
     ],
     description: "Eine Auswahl an Bildern von Hochtouren der letzten 5 Jahre",
     date: "2025-06-15"
