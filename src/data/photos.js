22b4f815ec088baa30fa6567f28c9bb24924b79a
Henri <w.henri@gmx.de>
2025-06-15 19:34:31 +0200

    Meine Änderungen

---
+++ src/data/photos.js
@@ -0,0 +1,37 @@
+export const photoSets = [
+  {
+    title: "Heidelberger Symposium",
+    slug: "symposium",
+    category: "events",
+    cover: "/bilder/portfolio/Symposium/Symp (5).jpg",
+    images: [
+    "/bilder/portfolio/Symposium/Symp (1).jpg",
+    "/bilder/portfolio/Symposium/Symp (2).jpg",
+    "/bilder/portfolio/Symposium/Symp (3).jpg",
+    "/bilder/portfolio/Symposium/Symp (4).jpg",
+    "/bilder/portfolio/Symposium/Symp (5).jpg",
+    "/bilder/portfolio/Symposium/Symp (6).jpg",
+    "/bilder/portfolio/Symposium/Symp (7).jpg",
+    "/bilder/portfolio/Symposium/Symp (8).jpg",
+    "/bilder/portfolio/Symposium/Symp (9).jpg"    
+    ],
+    description: "Das xx. Heidelberger Symposium an dessen Planung ich als Teil des Orga-Teams mitgewirkt habe.",
+    date: "2025-05-22"
+  },
+  {
+    title: "Auswahl Bergsteigen",
+    slug: "auswahl-bergsteigen",
+    category: "outdoor",
+    cover: "/bilder/portfolio/Bergsteigen/Bergsteigen (3).jpg",
+    images: [
+      "/bilder/portfolio/Bergsteigen/Bergsteigen (1).jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen (2).jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen (3).jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen (4).jpg",
+      "/bilder/portfolio/Bergsteigen/Bergsteigen (5).jpg"      
+    ],
+    description: "Eine Auswahl an Bildern von Hochtouren der letzten 5 Jahre",
+    date: "2025-06-15"
+  },
+
+];
